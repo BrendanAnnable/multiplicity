709d403d954c3ba2eb39bb969f71e5cb1b9f5beb
ashatch@gmail.com <ashatch@gmail.com@1863317a-5360-74fb-e71a-b4b166045bd1>
2010-05-18 23:02:30 +0000

    initial import

---
+++ multiplicity-config/trunk/src/main/java/multiplicity/config/table/TableConfigPrefsItem.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2009 University of Durham, England
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of 'SynergySpace' nor the names of its contributors 
+ *   may be used to endorse or promote products derived from this software 
+ *   without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package multiplicity.config.table;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.prefs.Preferences;
+
+import javax.swing.JComboBox;
+import javax.swing.JPanel;
+
+import multiplicity.config.ConfigurationApplication;
+import multiplicity.config.PreferencesItem;
+
+public class TableConfigPrefsItem implements PreferencesItem {
+
+	private static final Preferences prefs = ConfigurationApplication.getPreferences(TableConfigPrefsItem.class);
+	
+	public static final String PREFS_TABLE_TYPE = "TABLE_TYPE";
+	
+	public static enum TableType {
+		JMEDIRECT, TUIOSIM, TUIO, LUMIN
+	}
+	
+	@Override
+	public JPanel getConfigurationPanel() {
+		JPanel panel = new JPanel();
+		final JComboBox jcb = new JComboBox(TableType.values());
+		jcb.setSelectedItem(getTableType());
+		jcb.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				setTableType(TableType.valueOf(jcb.getSelectedItem().toString()));
+			}			
+		});
+		panel.add(jcb);
+		return panel;
+	}
+
+	@Override
+	public String getConfigurationPanelName() {
+		return "Table Type";
+	}
+	
+	public void setTableType(TableType type) {
+		prefs.put(PREFS_TABLE_TYPE, type.name());
+	}
+
+	public TableType getTableType() {			
+		return TableType.valueOf(prefs.get(PREFS_TABLE_TYPE, TableType.JMEDIRECT.name()));
+	}
+}
