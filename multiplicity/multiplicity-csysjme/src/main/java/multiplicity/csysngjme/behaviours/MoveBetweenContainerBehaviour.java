9f8c93b986e0e6cf6a7f0d4b5d1b6e232cb111c0
jt151077@gmail.com <jt151077@gmail.com@1863317a-5360-74fb-e71a-b4b166045bd1>
2010-06-02 11:25:38 +0000

    new behavior (empty for now), but which will be used as soon as we get hold of the "stage"

---
+++ multiplicity/multiplicity-csysjme/src/main/java/multiplicity/csysngjme/behaviours/MoveBetweenContainerBehaviour.java
@@ -0,0 +1,70 @@
+package multiplicity.csysngjme.behaviours;
+
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+import com.jme.system.DisplaySystem;
+
+import multiplicity.csysng.behaviours.IBehaviour;
+import multiplicity.csysng.items.IItem;
+import multiplicity.csysngjme.items.JMEItem;
+import multiplicity.csysngjme.picking.AccuratePickingUtility;
+import multiplicity.csysngjme.picking.PickedSpatial;
+import multiplicity.input.IMultiTouchEventListener;
+import multiplicity.input.events.MultiTouchCursorEvent;
+import multiplicity.input.events.MultiTouchObjectEvent;
+
+public class MoveBetweenContainerBehaviour implements IBehaviour, IMultiTouchEventListener {
+	
+	private final static Logger logger = Logger.getLogger(MoveBetweenContainerBehaviour.class.getName());	
+	private IItem item;
+
+	@Override
+	public void setItemActingOn(IItem item) {
+		this.item = item;
+		item.getMultiTouchDispatcher().addListener(this);
+	}
+
+	@Override
+	public void cursorChanged(MultiTouchCursorEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void cursorClicked(MultiTouchCursorEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void cursorPressed(MultiTouchCursorEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void cursorReleased(MultiTouchCursorEvent event) {
+		logger.info("cursor released caught event: "+item.getParentItem().getClass());
+	}
+
+	@Override
+	public void objectAdded(MultiTouchObjectEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void objectChanged(MultiTouchObjectEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void objectRemoved(MultiTouchObjectEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
