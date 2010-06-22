4551ced5df0d7f6ef6405830211743ec5278f0a5
jt151077@gmail.com <jt151077@gmail.com@1863317a-5360-74fb-e71a-b4b166045bd1>
2010-06-22 11:29:41 +0000

    hotspot repo

---
+++ multiplicity/multiplicity-appgallery/src/main/java/multiplicity/appgallery/stitcher/HotSpotFrame.java
@@ -0,0 +1,31 @@
+package multiplicity.appgallery.stitcher;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+import multiplicity.csysng.items.IColourRectangle;
+import multiplicity.csysng.items.IItem;
+import multiplicity.csysngjme.items.JMEFrame;
+
+public class HotSpotFrame extends JMEFrame {
+	
+	private static final long serialVersionUID = 8114328886119432460L;
+	
+	protected ArrayList<IColourRectangle> hotSpots = new ArrayList<IColourRectangle>(); 
+
+	public HotSpotFrame(String name, UUID uuid, int width, int height) {
+		super(name, uuid, width, height);
+	}
+
+	public ArrayList<IColourRectangle> getHotSpots() {
+		return hotSpots;
+	}
+	
+	public void setHotSpots(ArrayList<IColourRectangle> hotSpots) {
+		this.hotSpots = hotSpots;
+	}
+
+	public void addHotSpot(IItem item) {
+		hotSpots.add((IColourRectangle) item);
+	}
+}
