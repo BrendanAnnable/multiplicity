b83f15f6dc6af34a3337353e572a4a2bd16e0cc8
aperritano <aperritano@1863317a-5360-74fb-e71a-b4b166045bd1>
2010-06-22 11:38:10 +0000

    test app for hotspots

---
+++ trunk/multiplicity/multiplicity-appgallery/src/main/java/multiplicity/appgallery/hotspot/GetAttachmentItems.java
@@ -0,0 +1,247 @@
+package multiplicity.appgallery.hotspot;
+
+import java.awt.Color;
+import java.awt.image.RenderedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.Vector;
+
+import javax.imageio.ImageIO;
+
+import org.apache.log4j.Logger;
+
+import com.jme.math.Vector2f;
+import com.jme.scene.Geometry;
+import com.jme.scene.Node;
+import com.jme.scene.Spatial;
+
+import multiplicity.app.singleappsystem.SingleAppTableSystem;
+import multiplicity.app.utils.LocalStorageUtility;
+import multiplicity.csysng.behaviours.BehaviourMaker;
+import multiplicity.csysng.gfx.Gradient;
+import multiplicity.csysng.gfx.Gradient.GradientDirection;
+import multiplicity.csysng.items.IFrame;
+import multiplicity.csysng.items.IImage;
+import multiplicity.csysng.items.IItem;
+import multiplicity.csysng.items.IImage.AlphaStyle;
+import multiplicity.csysng.items.events.ItemListenerAdapter;
+import multiplicity.csysngjme.behaviours.MoveBetweenContainerBehaviour;
+import multiplicity.csysngjme.behaviours.RotateTranslateScaleBehaviour;
+import multiplicity.csysngjme.items.JMEFrame;
+import multiplicity.csysngjme.items.JMEItem;
+import multiplicity.csysngjme.items.JMERoundedRectangleBorder;
+import multiplicity.csysngjme.picking.AccuratePickingUtility;
+import multiplicity.csysngjme.picking.PickedSpatial;
+import multiplicity.input.data.CursorPositionRecord;
+import multiplicity.input.events.MultiTouchCursorEvent;
+import multiplicity.jmeutils.UnitConversion;
+import no.uio.intermedia.snomobile.WikiUtility;
+import no.uio.intermedia.snomobile.interfaces.IAttachment;
+import no.uio.intermedia.snomobile.interfaces.IPage;
+
+public class GetAttachmentItems extends Thread {
+
+	private final static Logger logger = Logger
+			.getLogger(GetAttachmentItems.class.getName());
+	private Object obj = new Object();
+	private Vector<IAttachment> attachments;
+	private List<IItem> items = null;
+	private IPage iPage = null;
+	private HotSpotApp hotspot;
+	private String parentContainerName;
+
+	public GetAttachmentItems(HotSpotApp stitcherApp, IPage iPage,
+			Vector<IAttachment> attachments, List<IItem> items,
+			String parentContainerName) {
+		this.hotspot = stitcherApp;
+		this.attachments = attachments;
+		this.iPage = iPage;
+		this.items = items;
+		this.parentContainerName = parentContainerName;
+	}
+
+	/**
+	 * Enables to write to a local directory, will which be created if
+	 * non-existant
+	 * 
+	 * @param id
+	 * @param org
+	 *            .dom4j.Document
+	 * @param Directory
+	 *            name
+	 * @param Version
+	 */
+	public File writeFileToLocalStorageDir(String filename, String dirName) {
+		String targetDirectory = LocalStorageUtility.getLocalWorkingDirectory(
+				hotspot.MULTIPLICITY_SPACE, "").getAbsolutePath()
+				+ File.separatorChar + dirName;
+		boolean canUpload = false;
+		File savedFile = null;
+		if (new File(targetDirectory).exists() == false) {
+			canUpload = (new File(targetDirectory)).mkdirs();
+		} else {
+			canUpload = true;
+		}
+
+		if (canUpload) {
+			hotspot.output_document_path = targetDirectory
+					+ File.separatorChar + filename;
+			savedFile = new File(hotspot.output_document_path);
+		}
+		return savedFile;
+	}
+
+	public List<IItem> getItems() {
+		return items;
+	}
+
+	public void run() {
+		for (IAttachment iAttachment : attachments) {
+
+			if (iAttachment.isOfImageType()) {
+				IImage img = null;
+				File file = writeFileToLocalStorageDir(iAttachment.getName(),
+						iPage.getPageName());
+
+				logger.info("File exists: " + file.exists() + " - "
+						+ iAttachment.getName() + " - " + iAttachment.getSize()
+						+ " - " + file.length());
+				if (file.exists() == false) {
+					WikiUtility wikiUtility = new WikiUtility(
+							hotspot.wikiUser, hotspot.wikiPass,
+							hotspot.maxFileSize, iAttachment.getMimeType(),
+							iAttachment.getAbsoluteUrl());
+					try {
+						wikiUtility.start();
+						wikiUtility.join();
+					} catch (InterruptedException e) {
+						logger
+								.debug("getComments:  InterruptedException: "
+										+ e);
+					}
+					Object resourceToDownload = wikiUtility.getResource();
+
+					if (resourceToDownload != null) {
+						iAttachment.setIsValid(true);
+						try {
+							ImageIO.write((RenderedImage) resourceToDownload,
+									iAttachment.getMimeType().substring(6),
+									file);
+						} catch (IOException e) {
+							logger.debug("IOException: " + e);
+						}
+					} else {
+						iAttachment.setIsValid(false);
+					}
+				} else {
+					iAttachment.setIsValid(true);
+					// TODO: check that the file on the disk is at least the
+					// same size as the "expected one", if not, download new.
+					// udpate: 31/05/2010 - Already attempted comparing
+					// iAttachment.getSize() and file.length(), but they appear
+					// not to be the same
+					// even though the files on disk are valid
+				}
+
+				if (iAttachment.getIsValid()) {
+					try {
+						img = hotspot.getContentFactory().createImage("photo",
+								UUID.randomUUID());
+						img.setImage(file.toURI().toURL());
+					} catch (MalformedURLException e) {
+						logger.debug("MalformedURLException: " + e);
+					}
+					img.setRelativeScale(0.8f);
+					img.setAlphaBlending(AlphaStyle.USE_TRANSPARENCY);
+					img.addItemListener(new ItemListenerAdapter() {
+						@Override
+						public void itemCursorPressed(IItem item,
+								MultiTouchCursorEvent event) {
+							logger.info("item pressed" + item.getBehaviours()
+									+ "parent: " + item.getParentItem());
+						}
+
+						@Override
+						public void itemCursorClicked(IItem item,
+								MultiTouchCursorEvent event) {
+							logger.info("item clicked" + item.getBehaviours());
+						}
+
+						@Override
+						public void itemCursorReleased(IItem item, MultiTouchCursorEvent event) {
+							moveItemToNewFrame(item, event);
+
+						}
+					});
+
+					BehaviourMaker.addBehaviour(img, RotateTranslateScaleBehaviour.class);
+					// BehaviourMaker.addBehaviour(img, MoveBetweenContainerBehaviour.class);
+
+					// stitcher.zOrderedItems.add(img);
+					items.add(img);
+				}
+			}
+		}
+	}
+
+    public void moveItemToNewFrame(IItem item, MultiTouchCursorEvent event) {
+        boolean offParent = true;
+
+        Node s = (Node) hotspot.getOrthoNode();
+
+        Vector2f locStore = new Vector2f();
+        UnitConversion.tableToScreen(event.getPosition().x, event.getPosition().y, locStore);
+
+        List<PickedSpatial> spatialsList = AccuratePickingUtility.pickAllOrthogonal(s.getParent().getParent(), locStore);
+
+        if (item.getParentItem().getTreeRootSpatial() instanceof JMEFrame) {
+        	JMEFrame frame = (JMEFrame) item.getParentItem().getTreeRootSpatial();
+        	for (PickedSpatial pickedSpatial : spatialsList) {
+        		if (pickedSpatial.getSpatial().equals(frame.getMaskGeometry())) {
+        			offParent = false;
+        		}
+        	}
+        }
+
+        if (parentContainerName.equals(hotspot.BACKGROUND_NAME) && offParent) {
+        	IFrame frame = (IFrame) item.getParentItem();
+        	frame.removeItem(item);
+        	hotspot.moveItemToNewFrame(item, new Vector2f(0.0f, 0.0f), "back-" + item.getUUID());
+        } else {
+        	// check if we are dropping on a "background" frame
+        	boolean firstFrameFound = false;
+        	for (PickedSpatial pickedSpatial : spatialsList) {
+        		if((pickedSpatial.getSpatial().toString()).equals("maskGeometry") && !firstFrameFound ) {
+        			try {
+        				Geometry geometry = (Geometry) pickedSpatial.getSpatial();
+        				JMEFrame targetFrame = (JMEFrame) geometry.getParent();
+        				
+        				if(targetFrame.getName().contains("back-")) {
+        					firstFrameFound = true;
+        					IFrame frame = (IFrame) item.getParentItem();
+        					frame.removeItem(item);
+        					
+        					Vector2f itemWorldPos = item.getWorldLocation();
+        					targetFrame.add(item);
+        			        item.setWorldLocation(itemWorldPos);
+        			        targetFrame.getZOrderManager().bringToTop(item, null);    
+        			        item.centerItem();
+        			        //BehaviourMaker.addBehaviour(item, RotateTranslateScaleBehaviour.class);
+        				}
+        				
+        			}
+        			catch (Exception e) {
+        				logger.debug("GetAttachmentItems: itemCursorReleased: Exception: "+e);
+        			}
+        		}
+        	}
+        }
+
+        logger.info("cursor released caught event: " + item.getParentItem().getClass());
+    }
+
+}
