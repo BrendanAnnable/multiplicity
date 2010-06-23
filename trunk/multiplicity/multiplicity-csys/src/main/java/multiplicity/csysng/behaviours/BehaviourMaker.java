90f347e78281e8c513a13d90ecec7cde2120b3b1
aperritano <aperritano@1863317a-5360-74fb-e71a-b4b166045bd1>
2010-06-23 13:09:00 +0000

    better formatting

--- trunk/multiplicity/multiplicity-csys/src/main/java/multiplicity/csysng/behaviours/BehaviourMaker.java
+++ trunk/multiplicity/multiplicity-csys/src/main/java/multiplicity/csysng/behaviours/BehaviourMaker.java
@@ -7,35 +7,35 @@ import java.util.logging.Logger;
 import multiplicity.csysng.items.IItem;
 
 public class BehaviourMaker {
-	
-	private static final Logger log = Logger.getLogger(BehaviourMaker.class.getName());
-	
-	public static IBehaviour addBehaviour(IItem item, Class<? extends IBehaviour> behaviourClass) {
-		try {
-			IBehaviour behaviour = behaviourClass.newInstance();
-			behaviour.setItemActingOn(item);
-			item.behaviourAdded(behaviour);
-			return behaviour;
-		} catch (InstantiationException e) {
-			log.warning(e.toString());
-		} catch (IllegalAccessException e) {
-			log.warning(e.toString());
-		}
-		return null;
-	}
-	
-	public static void removeBehavior(IItem item, Class<? extends IBehaviour> behaviourClass) {
-	     if( !item.getBehaviours().isEmpty() ) {
-	         List<IBehaviour> behaviours = item.getBehaviours();
-	         IBehaviour newInstance;
-                for (IBehaviour be : behaviours) {
-                    if( be.getClass().equals(behaviourClass) ) {
-                        item.getBehaviours().remove(be);
-                    }
-               }
-	         
-	        
-	         
-	     }
-	}
+
+    private static final Logger log = Logger.getLogger(BehaviourMaker.class
+            .getName());
+
+    public static IBehaviour addBehaviour(IItem item,
+            Class<? extends IBehaviour> behaviourClass) {
+        try {
+            IBehaviour behaviour = behaviourClass.newInstance();
+            behaviour.setItemActingOn(item);
+            item.behaviourAdded(behaviour);
+            return behaviour;
+        } catch (InstantiationException e) {
+            log.warning(e.toString());
+        } catch (IllegalAccessException e) {
+            log.warning(e.toString());
+        }
+        return null;
+    }
+
+    public static void removeBehavior(IItem item,
+            Class<? extends IBehaviour> behaviourClass) {
+        if (!item.getBehaviours().isEmpty()) {
+            List<IBehaviour> behaviours = item.getBehaviours();
+            for (IBehaviour be : behaviours) {
+                if (be.getClass().equals(behaviourClass)) {
+                    item.getBehaviours().remove(be);
+                }
+            }
+
+        }
+    }
 }
