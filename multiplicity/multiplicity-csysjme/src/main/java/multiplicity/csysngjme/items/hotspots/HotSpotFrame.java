2f9659cf02bb27ddf09df070bc773d84b2bfb92e
jt151077@gmail.com <jt151077@gmail.com@1863317a-5360-74fb-e71a-b4b166045bd1>
2010-06-25 06:51:47 +0000

    added removing the lines before a redraw when moving a hotspot

--- multiplicity/multiplicity-csysjme/src/main/java/multiplicity/csysngjme/items/hotspots/HotSpotFrame.java
+++ multiplicity/multiplicity-csysjme/src/main/java/multiplicity/csysngjme/items/hotspots/HotSpotFrame.java
@@ -3,6 +3,12 @@ package multiplicity.csysngjme.items.hotspots;
 import java.util.ArrayList;
 import java.util.UUID;
 
+import com.jme.math.Vector2f;
+import com.jme.math.Vector3f;
+import com.jme.renderer.ColorRGBA;
+import com.jme.scene.Line;
+import com.jme.scene.Line.Mode;
+
 import multiplicity.csysng.items.IItem;
 import multiplicity.csysng.items.hotspot.IHotSpotItem;
 import multiplicity.csysngjme.items.JMEFrame;
@@ -12,6 +18,7 @@ public class HotSpotFrame extends JMEFrame {
 	private static final long serialVersionUID = 8114328886119432460L;
 	
 	protected ArrayList<IHotSpotItem> hotSpots = new ArrayList<IHotSpotItem>(); 
+	protected ArrayList<Line> lines = new ArrayList<Line>(); ;
 
 	public HotSpotFrame(String name, UUID uuid, int width, int height) {
 		super(name, uuid, width, height);
@@ -33,7 +40,28 @@ public class HotSpotFrame extends JMEFrame {
 
 	public void bringHotSpotsToTop() {
 		for (IHotSpotItem iHotSpotItem : hotSpots) {
-			this.getZOrderManager().bringToTop(iHotSpotItem, null);   
+			this.getZOrderManager().bringToTop(iHotSpotItem, null);  
 		}
+	}
+
+	public void connectHotSpots(IHotSpotItem ihotSpotItem1, IHotSpotItem ihotSpotItem2) {
+		// TODO Auto-generated method stub
+		if(lines.size() > 0) {
+			this.detachChild(lines.get(0));
+			lines = new ArrayList<Line>();
+		}
+		
+		Vector2f xyHS1 = ihotSpotItem1.getRelativeLocation();
+		Vector2f xyHS2 = ihotSpotItem2.getRelativeLocation();
+		
+		Vector3f[] vertices = new Vector3f[2];
+		vertices[0] = new Vector3f(xyHS1.x, xyHS1.y, 0f);
+		vertices[1] = new Vector3f(xyHS2.x, xyHS2.y, 0f);
+		Line line = new Line("link", vertices, null, null, null);
+		line.setMode(Mode.Connected);
+		line.setLineWidth(3f);
+		line.setSolidColor(ColorRGBA.red);		
+		this.attachChild(line);
+		lines.add(line);
 	}
 }
