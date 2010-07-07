b8f7055a140a2806244a2ece2e25f219ff0749c6
aperritano <aperritano@1863317a-5360-74fb-e71a-b4b166045bd1>
2010-07-07 09:37:10 +0000

    fixed overlap taps SNOW-170

--- multiplicity/multiplicity-csys/src/main/java/multiplicity/csysng/items/IPalet.java
+++ multiplicity/multiplicity-csys/src/main/java/multiplicity/csysng/items/IPalet.java
@@ -3,5 +3,9 @@ package multiplicity.csysng.items;
 public interface IPalet extends IItem {
 
 	public void updatePalet(boolean locked);
+	
+	public int tap();
+	
+	public void resetTaps();
 
 }
--- multiplicity/multiplicity-csysjme/src/main/java/multiplicity/csysngjme/items/JMEPalet.java
+++ multiplicity/multiplicity-csysjme/src/main/java/multiplicity/csysngjme/items/JMEPalet.java
@@ -9,6 +9,7 @@ import com.jme.renderer.ColorRGBA;
 public class JMEPalet extends JMEColourCircle implements IPalet {
 
 	private static final long serialVersionUID = 8754761957677002270L;
+	private int taps = 0;
 	
 	public JMEPalet(String name, UUID uuid, float radius) {
 		super(name, uuid, radius);
@@ -33,4 +34,14 @@ public class JMEPalet extends JMEColourCircle implements IPalet {
 			super.changeBackgroundColor(new ColorRGBA(0f, 1f, 0f, 1f));
 		}
 	}
+
+    @Override
+    public int tap() {
+        return taps++;
+    }
+
+    @Override
+    public void resetTaps() {
+        taps = 0;
+    }
 }
