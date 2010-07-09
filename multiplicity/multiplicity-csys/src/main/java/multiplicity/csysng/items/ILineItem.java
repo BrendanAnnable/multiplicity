e844e0df08b5aa1fffa564c1951efe0392ec11f4
aperritano <aperritano@1863317a-5360-74fb-e71a-b4b166045bd1>
2010-07-09 14:59:30 +0000

    extends IItem now

--- multiplicity/multiplicity-csys/src/main/java/multiplicity/csysng/items/ILineItem.java
+++ multiplicity/multiplicity-csys/src/main/java/multiplicity/csysng/items/ILineItem.java
@@ -5,7 +5,7 @@ import multiplicity.csysng.items.hotspot.IHotSpotItem;
 import com.jme.math.Vector2f;
 import com.jme.math.Vector3f;
 
-public interface ILineItem {
+ public interface ILineItem extends IItem{
 	public void setSize(float width, float height);
 	public void setSize(Vector2f size);
 	public Vector2f getSize();
