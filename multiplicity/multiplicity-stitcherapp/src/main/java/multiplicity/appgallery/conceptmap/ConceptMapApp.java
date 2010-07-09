fb0594cfd5ced067c55e367a065d7ca7e2714336
aperritano <aperritano@1863317a-5360-74fb-e71a-b4b166045bd1>
2010-07-09 12:13:18 +0000

    moving to a new project

---
+++ multiplicity/multiplicity-stitcherapp/src/main/java/multiplicity/appgallery/conceptmap/ConceptMapApp.java
@@ -0,0 +1,121 @@
+package multiplicity.appgallery.conceptmap;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.lang.reflect.InvocationTargetException;
+import java.util.UUID;
+
+import multiplicity.app.singleappsystem.AbstractStandaloneApp;
+import multiplicity.app.singleappsystem.SingleAppTableSystem;
+import multiplicity.appgallery.gallery.GalleryApp;
+import multiplicity.csysng.animation.AnimationSystem;
+import multiplicity.csysng.behaviours.BehaviourMaker;
+import multiplicity.csysng.behaviours.gesture.GestureDetectionBehaviour;
+import multiplicity.csysng.behaviours.gesture.GestureLibrary;
+import multiplicity.csysng.behaviours.gesture.GestureMatch;
+import multiplicity.csysng.behaviours.gesture.IGestureListener;
+import multiplicity.csysng.gfx.Gradient;
+import multiplicity.csysng.gfx.Gradient.GradientDirection;
+import multiplicity.csysng.items.IFrame;
+import multiplicity.csysng.items.IImage;
+import multiplicity.csysng.items.IItem;
+import multiplicity.csysng.items.ILabel;
+import multiplicity.csysngjme.animation.animelements.Fader;
+import multiplicity.csysngjme.behaviours.RotateTranslateScaleBehaviour;
+import multiplicity.csysngjme.items.JMERoundedRectangleBorder;
+import multiplicity.input.IMultiTouchEventProducer;
+
+import com.jme.math.Vector2f;
+import com.jme.scene.Geometry;
+
+public class ConceptMapApp extends AbstractStandaloneApp {
+
+    /**
+     * test again
+     * 
+     * @param producer
+     */
+	public ConceptMapApp(IMultiTouchEventProducer producer) {
+		super(producer);
+	}
+
+	@Override
+	public void onAppStart() {
+		addInstruction();
+		createGesturableBackground();
+	}
+	
+	/**
+	 * adds instructions
+	 * 
+	 */
+	private void addInstruction() {
+		ILabel instruction = contentFactory.createLabel("instruction", UUID.randomUUID());
+		instruction.setText("Draw a C shape, but in reverse...");
+		instruction.setFont(new Font("Arial Narrow", Font.PLAIN, 14));
+		instruction.setRelativeLocation(new Vector2f(0, 0));
+		instruction.setTextColour(Color.white);
+		add(instruction);
+		zOrderManager.bringToTop(instruction, null);
+		AnimationSystem.getInstance().add(new Fader((Geometry)instruction.getManipulableSpatial(), Fader.MODE_FADE_IN, 2, 10));
+	}
+
+	protected void createNewNode() {
+		IFrame frame = contentFactory.createFrame("randomframe", UUID.randomUUID(), 160, 70);		
+		frame.setBorder(new JMERoundedRectangleBorder("frameborder", UUID.randomUUID(), 10f, 8));
+		frame.setGradientBackground(new Gradient(
+				new Color(0.5f, 0.5f, 0.5f, 1f), 
+				new Color(0f, 0f, 0f,1f), GradientDirection.VERTICAL));
+		add(frame);
+		BehaviourMaker.addBehaviour(frame, RotateTranslateScaleBehaviour.class);
+
+		ILabel label2 = contentFactory.createLabel("label", UUID.randomUUID());
+		label2.setText("<New Node>");
+		label2.setFont(new Font("Arial", Font.PLAIN, 18));
+		label2.setTextColour(Color.white);
+		frame.addItem(label2);
+		label2.centerItem();
+		
+		zOrderManager.bringToTop(frame, null);
+	}
+	
+	private void createGesturableBackground() {
+		IImage bg = contentFactory.createImage("backgroundimage", UUID.randomUUID());
+		bg.setImage(GalleryApp.class.getResource("yellowflowers_1680x1050.png"));
+		bg.centerItem();
+		add(bg);		
+		zOrderManager.sendToBottom(bg, null);
+		zOrderManager.neverBringToTop(bg);
+		
+		GestureLibrary.getInstance().loadGesture("circle");
+		GestureLibrary.getInstance().loadGesture("line");
+
+		GestureDetectionBehaviour gdb = (GestureDetectionBehaviour) BehaviourMaker.addBehaviour(bg, GestureDetectionBehaviour.class);
+		gdb.addListener(new IGestureListener() {				
+			@Override
+			public void gestureDetected(GestureMatch match, IItem item) {				
+				if(match.libraryGesture.getName().equals("circle") && match.matchScore > 0.8) {
+					createNewNode();						
+				}
+			}
+		});
+	}
+	
+
+
+	/**
+	 * This is the main method
+	 * 
+	 * @param args
+	 * @throws SecurityException
+	 * @throws IllegalArgumentException
+	 * @throws NoSuchMethodException
+	 * @throws InstantiationException
+	 * @throws IllegalAccessException
+	 * @throws InvocationTargetException
+	 */
+	public static void main(String[] args) throws SecurityException, IllegalArgumentException, NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
+		SingleAppTableSystem.startSystem(ConceptMapApp.class);
+	}
+
+}
---
+++ multiplicity/multiplicity-stitcherapp/src/main/java/multiplicity/appgallery/stitcher/Grouper.java
@@ -0,0 +1,173 @@
+package multiplicity.appgallery.stitcher;
+
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import multiplicity.app.singleappsystem.AbstractStandaloneApp;
+import multiplicity.app.utils.XMLOperations;
+import multiplicity.csysng.behaviours.BehaviourMaker;
+import multiplicity.csysng.gfx.Gradient;
+import multiplicity.csysng.gfx.Gradient.GradientDirection;
+import multiplicity.csysng.items.IFrame;
+import multiplicity.csysng.items.IItem;
+import multiplicity.csysngjme.behaviours.RotateTranslateScaleBehaviour;
+import multiplicity.csysngjme.items.JMERoundedRectangleBorder;
+import multiplicity.input.IMultiTouchEventListener;
+import multiplicity.input.data.CursorPositionRecord;
+import multiplicity.input.events.MultiTouchCursorEvent;
+import multiplicity.input.events.MultiTouchObjectEvent;
+
+import org.apache.log4j.Logger;
+
+import com.jme.math.Vector2f;
+
+public class Grouper implements IMultiTouchEventListener {
+	
+	private static final Logger logger = Logger.getLogger(Grouper.class.getName());
+	
+	private List<IItem> registeredItems = new ArrayList<IItem>();
+	private AbstractStandaloneApp app;
+
+	public Grouper() {
+	}
+
+	@Override
+	public void cursorChanged(MultiTouchCursorEvent event) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void cursorClicked(MultiTouchCursorEvent event) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void cursorPressed(MultiTouchCursorEvent event) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void cursorReleased(MultiTouchCursorEvent event) {
+		List<CursorPositionRecord> history = event.getPositionHistory();
+
+		List<IItem> selectedItems = new ArrayList<IItem>();
+
+		for (IItem item : registeredItems) {
+			boolean selected = contains(history, new Vector2f(item
+					.getWorldLocation()), 50, 50, 100);
+			if (selected) {
+				selectedItems.add(item);
+				System.out.println(item.getManipulableSpatial().getName()
+						+ " is selected!");
+			}
+		}
+
+		if (!selectedItems.isEmpty() /* && selectedItems.size() > 1 */) {
+			
+			this.addItemsToFrame(selectedItems, event.getPosition());
+
+		}
+
+	}
+
+	@Override
+	public void objectAdded(MultiTouchObjectEvent event) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void objectChanged(MultiTouchObjectEvent event) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void objectRemoved(MultiTouchObjectEvent event) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void register(IItem item) {
+		if (!registeredItems.contains(item))
+			registeredItems.add(item);
+	}
+
+	public boolean contains(List<CursorPositionRecord> history, Vector2f p0,
+			float minWidth, float minHeight, float startEndTolerance) {
+		Vector2f ti, ti1;
+		int crossings = 0;
+		float minX = Float.MAX_VALUE;
+		float minY = Float.MAX_VALUE;
+		float maxX = Float.MIN_VALUE;
+		float maxY = Float.MIN_VALUE;
+		for (int i = 0; i < history.size() - 1; i++) {
+			ti = history.get(i).getPosition();
+			// min values
+			if (ti.x < minX)
+				minX = ti.x;
+			if (ti.y < minY)
+				minY = ti.y;
+			// max values
+			if (ti.x > maxX)
+				maxX = ti.x;
+			if (ti.y > maxY)
+				maxY = ti.y;
+
+			ti1 = history.get(i + 1).getPosition();
+			double slope = (ti1.x - ti.x) / (ti1.y - ti.y);
+			boolean cond1 = (ti.y <= p0.y) && (p0.y < ti1.y);
+			boolean cond2 = (ti1.y <= p0.y) && (p0.y < ti.y);
+			boolean cond3 = p0.x < slope * (p0.y - ti.y) + ti.x;
+			if ((cond1 || cond2) && cond3)
+				crossings++;
+		}
+		float width = maxX - minX;
+		float height = maxY - minY;
+		float startEndDistance = history.get(0).getPosition().distance(
+				history.get(history.size() - 1).getPosition());
+		return (crossings % 2 != 0) && (width > minWidth)
+				&& (height > minHeight)
+				&& (startEndDistance < startEndTolerance);
+	}
+
+	public void register(IItem item, AbstractStandaloneApp app) {
+		if (!registeredItems.contains(item))
+			registeredItems.add(item);
+		this.app = app;
+	}
+
+	public void addItemsToFrame(List<IItem> items, Vector2f atPosition) {
+		int frameWidth = 600;
+		int frameHeight = 600;
+		UUID uUID = UUID.randomUUID();
+		IFrame frame = app.getContentFactory().createFrame("group", uUID, frameWidth, frameHeight);
+		logger.info("Group UUID: "+uUID.toString());
+		frame.setBorder(new JMERoundedRectangleBorder("randomframeborder", UUID.randomUUID(), 10f, 15));
+		frame.setGradientBackground(new Gradient(new Color(0.5f, 0.5f, 0.5f, 0.8f), new Color(0f, 0f, 0f, 0.8f), GradientDirection.VERTICAL));
+		frame.maintainBorderSizeDuringScale();
+		frame.setRelativeLocation(atPosition);
+		BehaviourMaker.addBehaviour(frame, RotateTranslateScaleBehaviour.class);
+
+		app.add(frame);
+		for (IItem item : items) {
+			Vector2f itemWorldPos = item.getWorldLocation();
+			app.remove(item);			
+			frame.addItem(item);
+			item.setWorldLocation(itemWorldPos);
+		}
+		app.getzOrderManager().bringToTop(frame, null);
+		
+		createXMLRepresentationForGroup(uUID, items);
+	}
+
+	private void createXMLRepresentationForGroup(UUID uUID, List<IItem> items) {
+		XMLOperations xmlOperations = new XMLOperations(uUID, items);
+	}
+
+}
