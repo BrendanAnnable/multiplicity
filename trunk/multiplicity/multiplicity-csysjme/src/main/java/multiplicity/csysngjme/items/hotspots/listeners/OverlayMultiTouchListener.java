573171b659d5bcabd11c5dbd53a513cabac9ba73
aperritano <aperritano@1863317a-5360-74fb-e71a-b4b166045bd1>
2010-09-06 12:02:19 +0000

    refactor for scale test SNOW-250

---
+++ trunk/multiplicity/multiplicity-csysjme/src/main/java/multiplicity/csysngjme/items/hotspots/listeners/OverlayMultiTouchListener.java
@@ -0,0 +1,53 @@
+package multiplicity.csysngjme.items.hotspots.listeners;
+
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+import multiplicity.csysng.items.IColourRectangle;
+import multiplicity.csysng.items.hotspot.IHotSpotFrame;
+import multiplicity.csysng.items.hotspot.IHotSpotItem;
+import multiplicity.input.MultiTouchEventAdapter;
+import multiplicity.input.events.MultiTouchCursorEvent;
+
+public class OverlayMultiTouchListener extends MultiTouchEventAdapter {
+
+    private final static Logger logger = Logger.getLogger(OverlayMultiTouchListener.class.getName());
+
+    private IColourRectangle overlay;
+
+    public OverlayMultiTouchListener(IColourRectangle overlay) {
+        this.overlay = overlay;
+        this.overlay.getMultiTouchDispatcher().addListener(this);
+    }
+    
+    
+    @Override
+    public void cursorPressed(MultiTouchCursorEvent event) {
+        super.cursorPressed(event);
+        logger.debug("overlay pressed");
+        IHotSpotFrame hotspotFrame = (IHotSpotFrame)overlay.getParentItem();
+        hotspotFrame.bringHotSpotsToTop();
+        hotspotFrame.bringPaletToTop();
+    }
+    
+    @Override
+    public void cursorChanged(MultiTouchCursorEvent event) {
+        super.cursorChanged(event);
+        logger.debug("overlay changed");
+        IHotSpotFrame hotspotFrame = (IHotSpotFrame)overlay.getParentItem();
+        
+        if( hotspotFrame.getName().contains("back")) {
+            List<IHotSpotItem> hotSpots = hotspotFrame.getHotSpots();
+            for (IHotSpotItem iHotSpotItem : hotSpots) {
+                iHotSpotItem.updateHotSpot();
+            }
+        } else {
+            
+            HotSpotUtils.updateHotSpots(hotspotFrame);
+
+        }
+        hotspotFrame.updateOverLay();
+        hotspotFrame.bringPaletToTop();
+    }
+}
