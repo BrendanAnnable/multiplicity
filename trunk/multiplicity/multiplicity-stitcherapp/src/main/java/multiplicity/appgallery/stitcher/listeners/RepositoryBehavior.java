d3d67f066829d9538856ddc9836fd86780180d76
aperritano <aperritano@1863317a-5360-74fb-e71a-b4b166045bd1>
2010-09-20 10:29:12 +0000

    hides unhides chain SNOW-228

--- trunk/multiplicity/multiplicity-stitcherapp/src/main/java/multiplicity/appgallery/stitcher/listeners/RepositoryBehavior.java
+++ trunk/multiplicity/multiplicity-stitcherapp/src/main/java/multiplicity/appgallery/stitcher/listeners/RepositoryBehavior.java
@@ -44,7 +44,7 @@ public class RepositoryBehavior extends MultiTouchEventAdapter implements
             }
         }
         
-        StitcherUtils.updateAllHotLinkConnections();
+//        StitcherUtils.updateAllHotLinkConnections();
         
     }
     
