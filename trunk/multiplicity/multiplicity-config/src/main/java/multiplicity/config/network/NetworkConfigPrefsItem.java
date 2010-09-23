100da4433eadc580ee9be2b4e768d70e4d204207
ashatch@gmail.com <ashatch@gmail.com@1863317a-5360-74fb-e71a-b4b166045bd1>
2010-09-23 15:10:00 +0000

    tantalising: commit of preference support for XMPP server, user, pass...

--- trunk/multiplicity/multiplicity-config/src/main/java/multiplicity/config/network/NetworkConfigPrefsItem.java
+++ trunk/multiplicity/multiplicity-config/src/main/java/multiplicity/config/network/NetworkConfigPrefsItem.java
@@ -14,10 +14,17 @@ public class NetworkConfigPrefsItem implements PreferencesItem {
 	private static final String HTTP_PROXY_HOST = "HTTP_PROXY_HOST";
 	private static final String HTTP_PROXY_PORT = "HTTP_PROXY_PORT";
 	private static final String HTTP_PROXY_ENABLED = "HTTP_PROXY_ENABLED";
+	
+	private static final String XMPP_SERVER_HOST = "XMPP_SERVER_HOST";
+	private static final String XMPP_SERVER_PORT = "XMPP_SERVER_PORT";
+	private static final String XMPP_ENABLED = "XMPP_ENABLED";
+	private static final String XMPP_USER = "XMPP_USER";
+	private static final String XMPP_PASSWORD = "XMPP_PASSWORD";
+	
 
 	@Override
 	public JPanel getConfigurationPanel() {
-		return new NetworkPrefsPanel(this);
+		return new NetworkPrefsPanel2(this);
 	}
 
 	@Override
@@ -47,5 +54,45 @@ public class NetworkConfigPrefsItem implements PreferencesItem {
 	
 	public boolean getProxyEnabled() {
 		return prefs.getBoolean(HTTP_PROXY_ENABLED, false);
+	}
+        
+	public void setXMPPHost(String host) {
+		prefs.put(XMPP_SERVER_HOST, host);
+	}
+	
+	public String getXMPPHost() {
+		return prefs.get(XMPP_SERVER_HOST, "");
+	}
+      
+	public void setXMPPPort(int port) {
+		prefs.putInt(XMPP_SERVER_PORT, port);
+	}
+	
+	public int getXMPPPort() {
+		return prefs.getInt(XMPP_SERVER_PORT, 5222);
+	}
+	
+	public void setXMPPEnabled(boolean isEnabled) {
+		prefs.putBoolean(XMPP_ENABLED, isEnabled);
+	}
+	
+	public boolean getXMPPEnabled() {
+		return prefs.getBoolean(XMPP_ENABLED, false);
+	}
+	
+	public void setXMPPUser(String user) {
+		prefs.put(XMPP_USER, user);
+	}
+	
+	public String getXMPPUser() {
+		return prefs.get(XMPP_USER, "");
+	}
+	
+	public void setXMPPPassword(String password) {
+		prefs.put(XMPP_PASSWORD, password);
+	}
+	
+	public String getXMPPPassword() {
+		return prefs.get(XMPP_PASSWORD, "");
 	}
 }
