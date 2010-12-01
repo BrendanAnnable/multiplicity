c3f3f4b5bdae545592a39f13264d9d4660b5590c
ashatch <ashatch@1863317a-5360-74fb-e71a-b4b166045bd1>
2010-12-01 15:53:58 +0000

    Share project "multiplicity-network-core" into jme3 branch

---
+++ branches/multiplicity-jme3/multiplicity/multiplicity-network-core/src/main/java/multiplicity/networkbase/contentsync/IContentSynchronisationManager.java
@@ -0,0 +1,9 @@
+package multiplicity.networkbase.contentsync;
+
+import multiplicity.csysng.items.INode;
+
+public interface IContentSynchronisationManager {
+
+	public void setRoot(INode node);
+
+}
