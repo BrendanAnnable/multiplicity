4840ecc2e0166674d3894f45003828fe9963bc74
ashatch <ashatch@1863317a-5360-74fb-e71a-b4b166045bd1>
2011-05-16 12:34:47 +0000

    M3 cleanup of stray System.outs. Also, JMEMutableLabelDelegate will warn if a font resource cannot be found (sometimes due to Eclipse misbehaving).

--- multiplicity3-jme3contentsystem/src/main/java/multiplicity3/jme3csys/items/mutablelabel/FontCache.java
+++ multiplicity3-jme3contentsystem/src/main/java/multiplicity3/jme3csys/items/mutablelabel/FontCache.java
@@ -2,11 +2,14 @@ package multiplicity3.jme3csys.items.mutablelabel;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Logger;
 
 import com.jme3.asset.AssetManager;
 import com.jme3.font.BitmapFont;
 
 public class FontCache {
+	private static final Logger log = Logger.getLogger(FontCache.class.getName());
+	
 	private static FontCache instance;
 
 	public static FontCache get() {
@@ -26,6 +29,12 @@ public class FontCache {
 		BitmapFont font = cacheMap.get(resource);
 		if(font == null) {
 			font = assetManager.loadFont(resource);
+			if(font == null) {
+				log.severe("Looks like you need to do a clean on the workspace:");
+				log.severe("  Couldn't find font resource " + resource);
+				log.severe("  Either this doesn't exist OR ");
+				log.severe("  Eclipse didn't copy it properly, and a clean will fix it.");
+			}
 			cacheMap.put(resource, font);
 		}
 		return font;
