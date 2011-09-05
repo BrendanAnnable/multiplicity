3bbd0c145cb113b107d256a20e80eed4a94af4d7
j.a.mcnaughton87@gmail.com <j.a.mcnaughton87@gmail.com@1863317a-5360-74fb-e71a-b4b166045bd1>
2011-09-05 13:08:33 +0000

    M3 Removed unneeded fields from container class.

--- branches/multiplicity3-parent/multiplicity3-contentsystem/src/main/java/multiplicity3/csys/items/container/IContainer.java
+++ branches/multiplicity3-parent/multiplicity3-contentsystem/src/main/java/multiplicity3/csys/items/container/IContainer.java
@@ -2,9 +2,4 @@ package multiplicity3.csys.items.container;
 
 import multiplicity3.csys.items.item.IItem;
 
-public interface IContainer extends IItem {
-	
-	public Object[] getInformation();
-	public void setInformation(Object[] info);
-
-}
+public interface IContainer extends IItem {}
