7602ebb2f9745ebb3203fe24385c6dd4e62f8a14
james.andrew.mcnaughton@gmail.com <james.andrew.mcnaughton@gmail.com@1863317a-5360-74fb-e71a-b4b166045bd1>
2014-04-15 13:33:30 +0000

    Multiplicity 3 - Ability to set image textures directly.

--- multiplicity3-contentsystem/src/main/java/multiplicity3/csys/items/image/IImage.java
+++ multiplicity3-contentsystem/src/main/java/multiplicity3/csys/items/image/IImage.java
@@ -2,10 +2,13 @@ package multiplicity3.csys.items.image;
 
 import java.io.File;
 
+import com.jme3.texture.Texture;
+
 import multiplicity3.csys.items.shapes.IRectangularItem;
 
 public interface IImage extends IRectangularItem {
 	public void setImage(String imageResoure);
+	public void setTexture(Texture tex);
 	public String getImage();
 	public void setSize(float width, float height);
 	public void setImage(File imageFile);
